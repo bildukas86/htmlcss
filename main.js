a960e7bd71ba85d26a203b0b8865240fc0f509de
bildukas86 <ekoncevicius@gmail.com>
2023-01-12 02:43:41 +0000

    sidenav
    rework finished

--- main.js
+++ main.js
@@ -1,63 +1,19 @@
 const  hamButton = document.getElementById('ham-button');
 const sideNav = document.getElementById('sideNav')
 const mainContainer = document.getElementById('main-container');
+const overlay = document.getElementById("overlay");
 
 
 hamButton.addEventListener('click', function () {
   // console.log("I clicked")
     hamButton.classList.toggle("change");
-    if(sideNav.style.display === "block"){
-      sideNav.style.display = "none";
-      mainContainer.style.left = "0";
-    }else {
-      sideNav.style.display = "block";
-      mainContainer.style.left = "-275px";
-    }
-
-
-})
-
-
-///////////////////////////////////////////////////////////////////////////
-//old
-////////////////////////
-//animated hamburger icon
-// function hamFunction(x) {
-//
-//   x.classList.toggle("change");
-//
-//   var s = document.getElementById('sideNav');
-//   var b = document.body;
-//   var m = document.getElementById('main-container');
-//
-//
-//     if (s.style.display === "block"){
-//      s.style.display = "none";
-//      b.style.overflow = "visible";
-//      b.style.left = "0";
-//      m.style.opacity = "1";
-//
-//     } else {
-//       s.style.display = "block";
-//       b.style.overflow = "hidden";
-//       b.style.left = "-275px";
-//       m.style.opacity = "0.3";
-//
-//
-//     }
-// }
-/////////////////////////////////////////////////////////////////////////
-
-
-//////////////////////////////////////////////////////////////////////////
-// function hamFunction(x){
-//   //add class
-//   x.classList.toggle("change");
-//
-//   // open/close navigation
-//   var n = document.getElementById('sideNav');
-//
-//   n.classList.toggle("vis-on");
-//
-// }
-/////////////////////////////////////////////////////////////////////////////////
+    sideNav.classList.toggle("side-nav-open");
+    mainContainer.classList.toggle("side-nav-main");
+    overlay.classList.toggle("overlay");
+});
+overlay.addEventListener('click', function () {
+  hamButton.classList.toggle("change");
+  sideNav.classList.toggle("side-nav-open");
+  mainContainer.classList.toggle("side-nav-main");
+  overlay.classList.toggle("overlay");
+});
