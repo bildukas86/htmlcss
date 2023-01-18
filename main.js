e260aa287a8d6d8fd1a881f1e00095961e3c7158
bildukas86 <ekoncevicius@gmail.com>
2023-01-18 12:45:56 +0000

    cookie settings btn toggle
    added switch style on selected btn

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ overlay.addEventListener('click', function () {
 //cookie pop-up
 const cookiePop = document.querySelector(".cookie-content");
 const changeCookieSettings = document.querySelector(".btn-settings");
-const acceptCookie = document.querySelector(".btn-accept");
+const acceptCookie = document.querySelectorAll(".btn-accept");
 const cookieSettings = document.querySelector(".cookie-settings");
 
 //check settings clicked
@@ -31,8 +31,12 @@ changeCookieSettings.addEventListener("click", function(){
 });
 
 //check accept clicked
-acceptCookie.addEventListener("click", function() {
+acceptCookie[0].addEventListener("click", function() {
   cookiePop.style.display = "none";
+  localStorage.setItem("cookieAccepted", "yes")
+});
+acceptCookie[1].addEventListener("click", function() {
+  cookieSettings.style.display = "none";
   localStorage.setItem("cookieAccepted", "yes")
 });
 
@@ -44,3 +48,54 @@ setTimeout(function (){
         cookiePop.style.display = "flex";
     }
 }, 1000)
+
+//getting all buttons in cookie settings block
+const cookieSettingsButtons = document.querySelectorAll(".cookies-container  button");
+
+//set active buttons
+cookieSettingsButtons[0].classList.add("btn-web-cookie");
+cookieSettingsButtons[1].classList.add("btn-light-cookie");
+cookieSettingsButtons[2].classList.add("btn-web-cookie");
+cookieSettingsButtons[3].classList.add("btn-light-cookie");
+cookieSettingsButtons[6].classList.add("btn-web-cookie");
+//
+
+/////////////////////////////////////////////////////////////
+//switch
+///////////////////////////////////////////////////////////
+cookieSettingsButtons[1].addEventListener('click', function(){
+  cookieSettingsButtons[0].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[1].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[1].classList.add("btn-web-cookie");
+  cookieSettingsButtons[0].classList.add("btn-light-cookie");
+})
+
+cookieSettingsButtons[0].addEventListener('click', function(){
+  cookieSettingsButtons[1].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[0].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[0].classList.add("btn-web-cookie");
+  cookieSettingsButtons[1].classList.add("btn-light-cookie");
+})
+///////////////////////////////////////////////////////////////
+
+cookieSettingsButtons[3].addEventListener('click', function(){
+  cookieSettingsButtons[2].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[3].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[3].classList.add("btn-web-cookie");
+  cookieSettingsButtons[2].classList.add("btn-light-cookie");
+})
+
+cookieSettingsButtons[2].addEventListener('click', function(){
+  cookieSettingsButtons[3].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[2].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[2].classList.add("btn-web-cookie");
+  cookieSettingsButtons[3].classList.add("btn-light-cookie");
+})
+
+////////////////////////////////////////////
+///cokie cookieSettings cancel clicked
+///////////////////////////////////////////
+cookieSettingsButtons[5].addEventListener('click', function(){
+  cookieSettings.style.display = "none";
+  cookiePop.style.display = "flex";
+})
