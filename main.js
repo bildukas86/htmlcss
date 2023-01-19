a33725cc29c1198803b4da72e655aee46623bfc5
bildukas86 <ekoncevicius@gmail.com>
2023-01-19 12:37:04 +0000

    add
    added cookie table

--- main.js
+++ main.js
@@ -57,7 +57,107 @@ cookieSettingsButtons[0].classList.add("btn-web-cookie");
 cookieSettingsButtons[1].classList.add("btn-light-cookie");
 cookieSettingsButtons[2].classList.add("btn-web-cookie");
 cookieSettingsButtons[3].classList.add("btn-light-cookie");
-cookieSettingsButtons[6].classList.add("btn-web-cookie");
+//////////////////////////
+//table cookieSettingsButtons
+/////////////////////////////
+
+
+cookieSettingsButtons[5].classList.add("btn-web-cookie");
+cookieSettingsButtons[6].classList.add("btn-light-cookie");
+cookieSettingsButtons[7].classList.add("btn-web-cookie");
+cookieSettingsButtons[8].classList.add("btn-light-cookie");
+cookieSettingsButtons[9].classList.add("btn-web-cookie");
+cookieSettingsButtons[10].classList.add("btn-light-cookie");
+cookieSettingsButtons[11].classList.add("btn-web-cookie");
+cookieSettingsButtons[12].classList.add("btn-light-cookie");
+cookieSettingsButtons[13].classList.add("btn-web-cookie");
+cookieSettingsButtons[14].classList.add("btn-light-cookie");
+cookieSettingsButtons[15].classList.add("btn-web-cookie");
+cookieSettingsButtons[16].classList.add("btn-light-cookie");
+
+////////
+// table btn switch
+///////
+
+cookieSettingsButtons[6].addEventListener('click', function(){
+  cookieSettingsButtons[5].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[6].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[6].classList.add("btn-web-cookie");
+  cookieSettingsButtons[5].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[8].addEventListener('click', function(){
+  cookieSettingsButtons[7].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[8].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[8].classList.add("btn-web-cookie");
+  cookieSettingsButtons[7].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[10].addEventListener('click', function(){
+  cookieSettingsButtons[9].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[10].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[10].classList.add("btn-web-cookie");
+  cookieSettingsButtons[9].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[12].addEventListener('click', function(){
+  cookieSettingsButtons[11].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[12].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[12].classList.add("btn-web-cookie");
+  cookieSettingsButtons[11].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[14].addEventListener('click', function(){
+  cookieSettingsButtons[13].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[14].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[14].classList.add("btn-web-cookie");
+  cookieSettingsButtons[13].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[16].addEventListener('click', function(){
+  cookieSettingsButtons[15].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[16].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[16].classList.add("btn-web-cookie");
+  cookieSettingsButtons[15].classList.add("btn-light-cookie");
+})
+////////^^^^^^^^^^^^^^^^^^^//////////////////
+////////
+/////////////////////////////////////////////
+cookieSettingsButtons[5].addEventListener('click', function(){
+  cookieSettingsButtons[6].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[5].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[5].classList.add("btn-web-cookie");
+  cookieSettingsButtons[6].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[7].addEventListener('click', function(){
+  cookieSettingsButtons[8].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[7].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[7].classList.add("btn-web-cookie");
+  cookieSettingsButtons[8].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[9].addEventListener('click', function(){
+  cookieSettingsButtons[10].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[9].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[9].classList.add("btn-web-cookie");
+  cookieSettingsButtons[10].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[11].addEventListener('click', function(){
+  cookieSettingsButtons[12].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[11].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[11].classList.add("btn-web-cookie");
+  cookieSettingsButtons[12].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[13].addEventListener('click', function(){
+  cookieSettingsButtons[14].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[13].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[13].classList.add("btn-web-cookie");
+  cookieSettingsButtons[14].classList.add("btn-light-cookie");
+})
+cookieSettingsButtons[15].addEventListener('click', function(){
+  cookieSettingsButtons[16].classList.remove("btn-web-cookie");
+  cookieSettingsButtons[15].classList.remove("btn-light-cookie");
+  cookieSettingsButtons[15].classList.add("btn-web-cookie");
+  cookieSettingsButtons[16].classList.add("btn-light-cookie");
+})
+///////////////////////////////
+///
+///////////////////////////////
+cookieSettingsButtons[18].classList.add("btn-web-cookie");
 //
 
 /////////////////////////////////////////////////////////////
@@ -95,7 +195,24 @@ cookieSettingsButtons[2].addEventListener('click', function(){
 ////////////////////////////////////////////
 ///cokie cookieSettings cancel clicked
 ///////////////////////////////////////////
-cookieSettingsButtons[5].addEventListener('click', function(){
+cookieSettingsButtons[17].addEventListener('click', function(){
   cookieSettings.style.display = "none";
   cookiePop.style.display = "flex";
+})
+
+
+/////////////////////////////////////////////////
+//table show/hide
+/////////////////////////////////////////////////
+const cookieSettingsTable = document.querySelector("table");
+
+cookieSettingsButtons[4].addEventListener('click', function(){
+  console.log("I clicked")
+  if (cookieSettingsTable.style.display === "table") {
+    cookieSettingsTable.style.display = "none";
+    cookieSettingsButtons[4].innerHTML = "Show detailed preferences";
+  }else {
+    cookieSettingsTable.style.display = "table";
+    cookieSettingsButtons[4].innerHTML = "Hide detailed preferences";
+  }
 })
