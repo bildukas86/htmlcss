bd99dbf6d07057493d9737ba052d525e915f1240
bildukas86 <ekoncevicius@gmail.com>
2023-01-24 01:34:23 +0000

    changed
    header now sticky when scrolling up

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const  hamButton = document.getElementById('ham-button');
 const sideNav = document.getElementById('sideNav')
 const mainContainer = document.getElementById('main-container');
 const overlay = document.getElementById("overlay");
-
+const headerUpDown = document.getElementById('header')
 
 hamButton.addEventListener('click', function () {
   // console.log("I clicked")
@@ -17,3 +17,33 @@ overlay.addEventListener('click', function () {
   mainContainer.classList.toggle("side-nav-main");
   overlay.classList.toggle("overlay");
 });
+
+
+var scrollableElement = document.body; //document.getElementById('scrollableElement');
+
+scrollableElement.addEventListener('wheel', checkScrollDirection);
+
+
+//track scroll up or down
+let position = 0;
+
+//sticky header
+function checkScrollDirection(event) {
+  if (checkScrollDirectionIsUp(event)) {
+
+    position = 1;
+
+    headerUpDown.style.position = "sticky";
+  } else {
+
+    position = 0;
+    headerUpDown.style.position = "unset";
+  }
+}
+
+function checkScrollDirectionIsUp(event) {
+  if (event.wheelDelta) {
+    return event.wheelDelta > 0;
+  }
+  return event.deltaY < 0;
+}
